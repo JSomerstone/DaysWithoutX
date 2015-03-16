69febd40f96d2b8ad1d0a16efa115e7942d7c907
Joona Somerkivi <joona.somerkivi@solinor.com>
2015-03-16 23:44:07 +0200

    Fixed bunch of .html.twigs

--- source/public/index.php
+++ source/public/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../../vendor/autoload.php';
-require_once __DIR__ . '/../memeloader.php';
+require_once __DIR__ . '/../JSomerstone/autoloader.php';
 
 use \Symfony\Component\HttpFoundation\Request,
     \DerAlex\Silex\YamlConfigServiceProvider as Config;
@@ -8,6 +8,9 @@ use \Symfony\Component\HttpFoundation\Request,
 $app = new Silex\Application();
 $app['debug'] = true;
 $app->register(new Config(__DIR__ . '/../../config/config.yml'));
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../view',
+));
 
 $twigLoader = new Twig_Loader_Filesystem(__DIR__ . '/../view');
 $twig = new Twig_Environment($twigLoader);
@@ -22,7 +25,10 @@ $app->get('/', function() use ($app, $twig)
         array(
             'title' => 'Pohjakuvat',
             'image_path' => '/img',
-            'view_path' => ''
+            'view_path' => '',
+            'url' => '/'
         )
     );
 });
+
+$app->run();
