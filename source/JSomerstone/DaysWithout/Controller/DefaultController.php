807522c68ccf20ff9e1eb18ccc3d5f8b551b1937
Joona Somerkivi <joona.somerkivi@solinor.com>
2015-04-20 18:04:37 +0200

    Updated deployment scripts, minor validation changes, front-page wrapped in error-catching

--- source/JSomerstone/DaysWithout/Controller/DefaultController.php
+++ source/JSomerstone/DaysWithout/Controller/DefaultController.php
@@ -7,20 +7,25 @@ class DefaultController extends BaseController
 
     public function indexAction()
     {
-        $this->applyToResponse(
-            array(
-                'loggedIn' =>       $this->isLoggedIn(),
-                'latest' =>         $this->getLatestCounters(),
-                'resentResets' =>   $this->getResentResets(),
-                'succession' =>     $this->getSuccession(),
-                'counter_title' =>  $this->getSession()->get('counter-title')
-            )
-        );
-        $this->getSession()->remove('counter-title');
-        return $this->render(
-            'default/index.html.twig',
-            $this->response
-        );
+        return $this->invokeMethod(function(){
+
+            $this->applyToResponse(
+                array(
+                    'loggedIn' =>       $this->isLoggedIn(),
+                    'latest' =>         $this->getLatestCounters(),
+                    'resentResets' =>   $this->getResentResets(),
+                    'succession' =>     $this->getSuccession(),
+                    'counter_title' =>  $this->getSession()->get('counter-title')
+                )
+            );
+            $this->getSession()->remove('counter-title');
+
+            return $this->render(
+                'default/index.html.twig',
+                $this->response
+            );
+
+        });
     }
 
     private function getLatestCounters()
